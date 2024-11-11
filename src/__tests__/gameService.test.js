d4b1cd92f26b492b76e109f4fecb4d55108bb4f9
MercedesBanales <mercibanales@hotmail.com>
2024-11-10 21:01:53 -0300

    play test passed

--- src/__tests__/gameService.test.js
+++ src/__tests__/gameService.test.js
@@ -52,4 +52,29 @@ describe('start', () => {
       expect(result).toBe(mockGameId);
     });
   });
+
+  describe('play', () => {
+    it('should update the card with the chosen number and return the play response', async () => {
+      const mockPlayerId = 'player1';
+      const mockGameId = 'game123';
+      const coord_x = 1;
+      const coord_y = 1;
+      const mockCard = {
+        gameId: mockGameId,
+        playerId: mockPlayerId,
+        card: generateMockCard()
+      };
+      mockCard.card[coord_x][coord_y] = 0; 
+      cardsService.setChosenNumber.mockResolvedValue(mockCard);
+
+      const result = await gameService.play(mockPlayerId, mockGameId, coord_x, coord_y);
+
+      expect(cardsService.setChosenNumber).toHaveBeenCalledWith(mockPlayerId, mockGameId, coord_x, coord_y);
+      expect(result).toEqual({
+        card: mockCard.card,
+        message: StatusType.PLAY,
+        winner: null
+      });
+    });
+  });
   
